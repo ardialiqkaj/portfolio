c80af91b12a4e651e1647345d0c36808f32a744a
ardialiqkaj <ardi.aliqkaj@gmail.com>
2022-11-12 20:58:43 +0100

    updated navbar for mobile

--- script.js
+++ script.js
@@ -40,14 +40,24 @@ function moveTop() {
   }
 }
 
+const formStatus = document.querySelector(".contact-form");
+const checkRemoved = formStatus.classList.contains("remove");
+const checkShown = formStatus.classList.contains("showed");
 
-let message;
-function showHide() {
-  if(message==1) {
-    document.getElementsByClassName("contact-form").style.display="inline";
-    return message=0
-  } else {
-    document.getElementsByClassName("contact-form").style.display="none";
-    return message=1
+function messageButton() {
+  if (checkRemoved == true) {
+    showTicket();
+  } else if (checkShown == true) {
+    removeTicket();
   }
+}
+
+function showTicket() {
+  const forms = document.querySelector(".contact-form");
+  forms.classList.remove("remove");
+}
+
+function removeTicket() {
+  const forms = document.querySelector(".contact-form");
+  forms.classList.add("showed");
 }
